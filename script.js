78a22fd05d3420b1fe3771e4077f93a7c80f92b1
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2022-10-24 21:08:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,20 +19,36 @@
             this.category = category;
         }
         updateContentItem(id, name, description, category){
-            /* for( let i = 0; i < this.length; i++){
-                if(item[i].id == this.id){
-                    this.name = item[i].name; */
-                    console.log(this);
-               /*  }
-            }   */
+            console.log("Method Called");
+            if(id === this.id){
+                this.id = id;
+                this.name = name;
+                this.description = description;
+                this.category = category;
+                document.getElementById("content-item-1").innerHTML = "<h2>Advil</h2>" + "<p>Headache Relief</p>" + "<div id='category'>Pills</div>" + "</div>";
+                console.log("Success!!");
+            }
+            else if(id !== this.id){
+                console.log("Error");
+            }  
         }
     }
 
-    let item = [new ContentItem(1, "Anusol", "Hemroid Medication", "Topical"),new ContentItem(1,"Goldbond", "Foot Powder", "Powder"), new ContentItem(3, "Buckley's", "Tastes awful, but it works", "Cough Syrup"), new ContentItem(4, "Cialis", "Ask your doctor", "Pills"), new ContentItem(5, "Tylenol", "Rapid pain relief!", "Pills")];
+    let item = [
+        new ContentItem(1, "Anusol", "Hemroid Medication", "Topical"),
+        new ContentItem(2,"Goldbond", "Foot Powder", "Powder"), 
+        new ContentItem(3, "Buckley's", "Tastes awful, but it works", "Cough Syrup"), 
+        new ContentItem(4, "Cialis", "Ask your doctor", "Pills"), 
+        new ContentItem(5, "Tylenol", "Rapid pain relief!", "Pills")
+    ];
     let contentDiv = document.getElementById("content-item-list");
-    for( let i = 0; i < item.length; i++){
-        item[i].updateContentItem(item.id, item.name, item.description, item.category);
-    }
-    item.forEach(item => contentDiv.innerHTML += "<div class='content-item-wrapper' id='content-item-"+item.id+"'>" + Object.values(item.id.toString())  + "<h4>" + Object.values(item.name).join("") + "</h4>" + "<p>" + Object.values(item.description).join("") + "</p>" + "<div id='category'>" + Object.values(item.category).join("") + "</div>" + "</div>");
+    item.forEach(item => contentDiv.innerHTML += "<div class='content-item-wrapper' id='content-item-"+item.id+"'>" + "<h2>" + Object.values(item.name).join("") + "</h2>" + "<p>" + Object.values(item.description).join("") + "</p>" + "<div id='category'>" + Object.values(item.category).join("") + "</div>" + "</div>");
     $(".content-item-wrapper").css({'border':'2px solid black', "margin":'0 auto', "margin-bottom":'5%', "width": '85%', "padding": '10px'});
+
+    $('#updateObject').on('click', function(){
+        item[0].updateContentItem(1, "Advil", "Headache Relief", "Pills");
+    });
+    $('#updateObjectFail').on('click', function(){
+        item[0].updateContentItem(67, "Advil", "Headache Relief", "Pills");
+    });
 /* }); */
